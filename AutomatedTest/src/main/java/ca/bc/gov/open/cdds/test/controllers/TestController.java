0eaa5b04d1ef551e74ad53de4fb8dd704db3a05e
ekwan <224302@nttdata.com>
2022-02-14 14:56:36 -0800

    clean up

--- AutomatedTest/src/main/java/ca/bc/gov/open/cdds/test/controllers/TestController.java
+++ AutomatedTest/src/main/java/ca/bc/gov/open/cdds/test/controllers/TestController.java
@@ -43,7 +43,7 @@ public class TestController {
         } else {
             HttpHeaders responseHeaders = new HttpHeaders();
             responseHeaders.add("Content-Type", "application/json");
-            return new ResponseEntity<String>("{\"status\": \"All tests passed\"}", HttpStatus.OK);
+            return new ResponseEntity<String>("{\"status\": \"All tests passed\"}", responseHeaders, HttpStatus.OK);
         }
     }
 
